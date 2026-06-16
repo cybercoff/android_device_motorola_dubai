b46fcb37d40e515c47a7514469ff5602da6e0fc4
DDM <munozdariod@gmail.com>
2026-06-15 22:14:43 -0300

    Revert "drop include microg"
    This reverts commit 3e928840e19bd162c68ce91e8b57b03a0a9ba4a3.

--- lineage_dubai.mk
+++ lineage_dubai.mk
@@ -14,6 +14,9 @@ $(call inherit-product, device/motorola/dubai/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Inherit some Microg stuff.
+$(call inherit-product, vendor/partner_gms/products/gms_minimal.mk)
+
 PRODUCT_NAME := lineage_dubai
 PRODUCT_DEVICE := dubai
 PRODUCT_MANUFACTURER := motorola
@@ -40,6 +43,9 @@ TARGET_SUPPORTS_QUICK_TAP := false
 
 # no usar webcam for camera (sec)
 TARGET_BUILD_DEVICE_AS_WEBCAM := false
+
+# ROM type
+LUNARIS_BUILD_TYPE := UNOFFICIAL
 
 # BCR - a tu criterio
 WITH_BCR := false
