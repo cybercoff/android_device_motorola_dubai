a881e3623c24b5500412de34af9dd2042b4791eb
DDM <55451802+cybercoff@users.noreply.github.com>
2026-07-01 13:45:48 -0300

    BringUp crdroid

--- lineage_dubai.mk
+++ lineage_dubai.mk
@@ -31,41 +31,13 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=motorola/dubai_g/dubai:14/U1RDS34.80-40-5-6/ba4fd-68ef4:user/release-keys \
     DeviceProduct=dubai_g
 
+## DDM BRINGUP
+TARGET_FACE_UNLOCK_SUPPORTED := false
+TARGET_SUPPORTS_QUICK_TAP := false
+TARGET_BUILD_DEVICE_AS_WEBCAM := false
+TARGET_ENABLE_BLUR := true
+TARGET_HAS_UDFPS := false
 
 # Remove unwanted packages
 PRODUCT_PACKAGES += \
     RemovePackages
-
-# no incluir face unlock
-TARGET_FACE_UNLOCK_SUPPORTED := false
-
-# descartar quick tap
-TARGET_SUPPORTS_QUICK_TAP := false
-
-# no usar webcam for camera (sec)
-TARGET_BUILD_DEVICE_AS_WEBCAM := false
-
-# BCR - a tu criterio
-WITH_BCR := false
-
-# Highend device, vale la pena
-TARGET_OPTIMIZED_DEXOPT := true
-
-# Blur - a gusto, no afecta funcionalidad
-TARGET_ENABLE_BLUR := true
-
-# dubai tiene UDFPS (under-display fingerprint)
-TARGET_CUSTOM_UDFPS := false
-
-# SurfaceFlinger boost - compatible con Snapdragon, útil
-SURFACE_FLINGER_BOOST := true
-
-TARGET_BOOT_ANIMATION_RES := 1080
-
-# Use aosp telephony
-TARGET_SUPPORTS_GOOGLE_TELEPHONY := false
-
-# X-Reality Engine display enhancement
-USE_REALITY_ENGINE := true
-
-TARGET_USE_WALLPAPERS := false
